5ab4cf097124e9523863e4b492ff6aca9f230e0f
Siti Nur Mu'arivah <sitinurmuarivah92@gmail.com>
2023-05-11 02:15:34 +0700

    add BallController
    add force using space and camera movement on click mouse button

---
+++ Assets/BallController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float force;
+
+    bool shoot;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (shoot)
+        {
+            shoot = false;
+            Vector3 direction = Camera.main.transform.forward;
+            direction.y = 0;
+            rb.AddForce(direction * force, ForceMode.Impulse);
+        }
+    }
+
+    public bool IsMove()
+    {
+        return rb.velocity != Vector3.zero;
+    }
+}
